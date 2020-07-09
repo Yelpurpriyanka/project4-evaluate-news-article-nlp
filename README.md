4f13d07559b8e3c6a614d048d68a81a7eb824d5a
Priyanka <43899740+Yelpurpriyanka@users.noreply.github.com>
2020-07-09 10:32:33 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# project4-evaluate-news-article-nlp
+# News Article NLP
+
+This frontend application uses Aylien NLP service to analyze text and news article links.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing
+purposes. Follow to classroom lessons upt lesson 4 for setting up basic settings for the project.
+
+## Prerequisites
+
+You need `node` and `npm` are must installed on your system to be able to run and build this project.
+
+## Installing
+
+To install project dependencies run:for node modules
+npm install
+
+## Deployment
+
+To create an optimized front-end build for development, run:
+npm run build
+
+
+To run the server in production mode, run:
+npm run start
+
+
+## Built With
+
+Express- NodeJS Back-End server
+Webpack - Front-End code bundler
+Yarn - Dependency management
+
---
+++ src/server/index.js
@@ -0,0 +1,41 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const Aylien = require("aylien_textapi");
+
+const PORT = 8007;
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("dist"));
+
+const textApi = new Aylien({
+  application_id: "68837b99",
+  application_key: "0c18469543844e03b477be1a884e87bc"
+});
+
+app.get("/", (req, res) => res.sendFile("index.html"));
+
+app.post("/api", (req, res) => {
+  const { text } = req.body;
+  console.log("Request to '/api' endpoint", text);
+  textApi.sentiment({ text }, (error, result, remaining) => {
+    console.log("Aylien Callback", result, remaining);
+    if(!error) res.send(result);
+  });
+});
+
+app.post("/article", (req, res) => {
+  const { text } = req.body;
+  console.log("Request to '/article' endpoint", text);
+  textApi.sentiment({ url: text }, (error, result, remaining) => {
+    console.log("Aylien Callback", result, remaining);
+    res.send(result);
+  });
+});
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
